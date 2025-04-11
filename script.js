fd4e774ad8ba2db9b604cc3c0f90ddf0bdd10222
KyKirma <pedrohenriquekourly@hotmail.com>
2025-04-10 23:52:47 -0300

    feat: Implement chatbot UI and functionality
    - Added HTML structure for chatbot interface including message input, file upload, and emoji picker.
    - Created CSS styles for chat layout, message formatting, and responsive design.
    - Developed JavaScript logic for handling user messages, bot responses, file uploads, and emoji selection.
    - Integrated API for generating bot responses based on user input.

---
+++ script.js
@@ -0,0 +1,163 @@
+const messageInput = document.querySelector('.message-input');
+const chatBody = document.querySelector('.chat-body');
+const sendMessageButton = document.querySelector('#send_message');
+const fileInput = document.querySelector('#file-input');
+const fileUploadWrapper = document.querySelector('.file-upload-wrapper');
+const fileCancelButton = document.querySelector('#file-cancel');
+
+const API_KEY = `AIzaSyBJwIPjrg1d33ZqPnAoobCG6JQOK1KGxuQ`
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
+
+const userData = {
+    message : null,
+    file : {
+        data: null,
+        mime_type: null
+    }
+}
+
+const chatHistory = [];
+
+// Cria o elemento da messagem
+const createMessageElement = (content, ...classes) => {
+    const div = document.createElement('div');
+    div.classList.add('message', ...classes);
+    div.innerHTML = content;
+    return div;
+}
+
+const generateBotResponse = async (incomingMessageDiv) => {
+    const messageElement = incomingMessageDiv.querySelector('.message-text');
+
+    chatHistory.push({
+        role: "user",
+        parts: [{ text: userData.message }, ...(userData.file.data ? [{ inline_data: userData.file }] : [])],
+    });
+
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            contents: chatHistory
+        })
+    };
+    
+    try {
+        const response = await fetch(API_URL, requestOptions);
+        const data = await response.json();
+
+        if(!response.ok) throw new Error(data.error.message);
+
+        const apiResponseText = data.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g, "$1").trim();
+        messageElement.innerHTML = apiResponseText;
+
+        chatHistory.push({
+            role: "model",
+            parts: [{ text: apiResponseText }]
+        });
+    
+    } catch (error) {
+        console.log(error);
+        messageElement.innerHTML = error.message;
+        messageElement.style.color = "red";
+    } finally {
+        userData.file = {};
+        incomingMessageDiv.classList.remove('thinking');
+        chatBody.scrollTo({ top: chatBody.scrollHeight, behavior: "smooth" });
+    }
+    
+}
+
+// Cuida das requisições 
+const handleOutgoingMessage = (e) => {
+    e.preventDefault();
+    userData.message = messageInput.value.trim();
+    messageInput.value = '';
+    fileUploadWrapper.classList.remove("file-uploaded");
+
+    const messageContent = `<div class="message-text"></div>
+                    ${userData.file.data ? `<img src="data:${userData.file.mime_type};base64,${userData.file.data}" class="attachment">` : ''}`;
+
+    const outgoingMessageDiv = createMessageElement(messageContent, 'user-message');
+    outgoingMessageDiv.querySelector(".message-text").textContent = userData.message;
+    chatBody.appendChild(outgoingMessageDiv);
+    chatBody.scrollTo({ top: chatBody.scrollHeight, behavior: "smooth" });
+
+    setTimeout(() => {
+        const messageContent = `<div class="bot-icon">
+                        <img src="/assets/images/bot.svg" alt="Bot Icon" class="bot-icon-image" style="filter: invert(100%);">
+                    </div>
+                    <div class="message-text">
+                        <div class="thinking-indicator">
+                            <div class="dot"></div>
+                            <div class="dot"></div>
+                            <div class="dot"></div>
+                        </div>
+                    </div>`;
+
+        const incomingMessageDiv = createMessageElement(messageContent, 'bot-message', 'thinking');
+        chatBody.appendChild(incomingMessageDiv);
+        chatBody.scrollTo({ top: chatBody.scrollHeight, behavior: "smooth" });
+        generateBotResponse(incomingMessageDiv); 
+    }, 600);
+}
+
+messageInput.addEventListener("keydown", (e) => {
+    const userMessage = e.target.value.trim();
+
+    if(e.key === "Enter" && userMessage) {
+       handleOutgoingMessage(e);
+    }
+});
+
+fileInput.addEventListener("change", (e) => {
+    const file = fileInput.files[0];
+
+    if(!file) return;
+
+    const reader = new FileReader();
+    reader.onload = (e) => {
+        fileUploadWrapper.querySelector("img").src = e.target.result;
+        fileUploadWrapper.classList.add("file-uploaded");
+        const base64String = e.target.result.split(",")[1];
+
+        userData.file = {
+            data: base64String,
+            mime_type: file.type
+        }
+
+        fileInput.value = '';
+    }
+
+    reader.readAsDataURL(file);
+});
+
+fileCancelButton.addEventListener("click", () => {
+    userData.file = {};
+    fileUploadWrapper.classList.remove("file-uploaded");
+});
+
+const picker = new EmojiMart.Picker({
+    theme: "light",
+    skinTonePosition: "none",
+    previewPosition: "none",
+    onClickOutside: (e) => {
+        if(e.target.id === "emoji-picker") {
+            document.body.classList.toggle("show-emoji-picker");
+        } else {
+            document.body.classList.remove("show-emoji-picker");
+        }
+    },
+    onEmojiSelect: (emoji) => {
+        const { selectionStart: start, selectionEnd: end} = messageInput;
+        messageInput.setRangeText(emoji.native, start, end, "end");
+        messageInput.focus();
+    }
+})
+
+document.querySelector(".chat-form").appendChild(picker);
+
+sendMessageButton.addEventListener("click", (e) => handleOutgoingMessage(e));
+document.querySelector("#file-upload").addEventListener("click", () => fileInput.click());
